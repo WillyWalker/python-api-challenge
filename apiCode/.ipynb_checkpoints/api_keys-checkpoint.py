4bf758cdb309f6de577ed2fcdb417bf9243fbf2e
W. H. Walker <w.h.walker@Ws-MacBook-Air-2.local>
2023-09-17 18:57:37 -0700

    api code

---
+++ apiCode/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "3dff7bdf4f344758740068cc8e5d7dc8"
+
+# Geoapify API Key
+geoapify_key = "389e525083f04d3a8a95d5017a8ab9ca"
---
+++ apiCode/api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+
+## I AM A FAKE KEY, REPLACE ME ##
+weather_api_key = "fmkINLSDfMmCVFb9RRgpd2HbRHsiKRNrQLmZ7QAPH49f7Vv9Cg2ZvArFcgtwXWVC"
+
+# Geoapify API Key
+
+## I AM A FAKE KEY, REPLACE ME ##
+geoapify_key = "s4GwZnalQjDEisu8TsPJ6vXd6S77W2LU6QoU3uElW8hW9hffTOncPXqaresWUwiR"
